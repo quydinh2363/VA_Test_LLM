b6afd8155fa1f779fbef8940a0e316ad4fc46216
Dinh Ngoc Quy <quy489066@gmail.com>
2025-08-25 21:48:53 +0700

    test gpt_oss

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-print('hello word!')
-print(1+1)
+from langchain_groq import ChatGroq
+from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+api_key = os.getenv("GROQ_API_KEY")
+
+loader = WebBaseLoader("https://owasp.org/Top10/")
+docs = loader.load()
+
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db = Chroma.from_documents(docs, embeddings)
+
+# Dùng Groq làm LLM
+llm = ChatGroq(model="openai/gpt-oss-20b",api_key=api_key)
+
+qa = RetrievalQA.from_chain_type(llm=llm, retriever=db.as_retriever())
+
+print(qa.run("""
+hello.
+"""))
